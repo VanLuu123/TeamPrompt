ca8b65046e3702cc093a2eb07c6c3a154c748820
VanLuu123 <53393660+VanLuu123@users.noreply.github.com>
2025-06-07 18:12:48 -0700

    updated vector file

--- backend/core/vector.py
+++ backend/core/vector.py
@@ -5,10 +5,10 @@ from typing import List, Dict, Any, Literal
 from document_processor import DocumentProcessor
 from embeddings import Embeddings
 from pinecone_vector_store import PineconeVectorStore
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
 
-API_KEY = load_dotenv('PINECONE_API_KEY')
+load_dotenv()
 
 Pinecone.init(
     api_key=API_KEY,
@@ -21,9 +21,27 @@ class PineconeVectorStorage:
         self.dimension = dimension 
         self.metric = metric
         
-        if index_name not in metric:
-            pinecone.create_index(index_name, dimension=dimension, metric=metric)
-        self.index = pinecone.Index(index_name)
+        api_key = os.environ.get('PINECONE_API_KEY')
+        if not api_key:
+            raise ValueError("PINECONE API KEY is required")
+        
+        self.pc = Pinecone(api_key=api_key)
+        
+        existing_indexes = [index.name for index in self.pc.list_indexes()]
+        
+        if index_name not in existing_indexes:
+            print(f"Creating new index: {index_name}")
+            self.pc.create_index(
+                name=index_name,
+                dimension=dimension,
+                metric=metric,
+                spec=ServerlessSpec(
+                    cloud='aws',  
+                    region='us-west-1'  
+                )
+            )
+        
+        self.index = self.pc.Index(index_name)
         
     def upsert_documents(self, embedded_documents: List[Dict[str,Any]]):
         vectors = [
@@ -37,12 +55,13 @@ class PineconeVectorStorage:
         self.index.upsert(vectors)
         
     def query(self, embedding: np.ndarray, top_k: int = 5):
-        result = self.index.query (
+        results = self.index.query (
             vector = embedding.tolist(),
             top_k = top_k,
             include_metadata = True,
+            include_values = False,
         )
-        return result['matches']
+        return results['matches']
 
 
 
